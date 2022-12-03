3d7677209143034a70fb0422f24a02d1a930d654
alvin1904 <alvin19official@gmail.com>
2022-12-03 15:16:23 +0530

    complete css till footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import react from 'react'
 function footer(){
 
 
-return <footer className='bg-600'>
+return <footer className='footer bg-600'>
     <h1>Boost your links today</h1>
     <button className='btn'>Get Started</button>
 </footer>;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,7 @@
 import react from 'react'
+import i1 from '../images/icon-brand-recognition.svg'
+import i2 from '../images/icon-detailed-records.svg'
+import i3 from '../images/icon-fully-customizable.svg'
 
 function main(){
 
@@ -24,30 +27,36 @@ return <main className='bg-200'>
 
     </div>
 
-    <div className='heading'>
+    <div className='heading vertical'>
         <h3>Advanced Statistics</h3>
-        <p className='content'>Track how your links are performing across the web with our advanced statistics dashboard    </p>
+        <p className='content'>Track how your links are performing across the web with our advanced statistics dashboard</p>
+        <div className='cards horizontal'>
+            <div className='card'>
+                <div className='img-holder'>
+                    <img src={i1}></img>
+                </div>
+                <h4 className='d'>Brand Recognition</h4>
+                <p className='content'>Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill a confidence in your content.</p>
+            </div>
+
+            <div className='card'>
+                <div className='img-holder'>
+                    <img src={i2}></img>
+                </div>
+                <h4 className='d'>Detailed Records</h4>
+                <p className='content'>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
+            </div>
+
+            <div className='card'>
+                <div className='img-holder'>
+                    <img src={i3}></img>
+                </div>
+                <h4 className='d'>Fully Customizable</h4>
+                <p className='content'>Improve brand awareness and content discoverability though customizable links, supercharging audience engagement</p>
+            </div>
     </div>
-    <div className='cards'>
-
-        <div className='card'>
-            <img src=""></img>
-            <h4 className='d'>Detailed analysis</h4>
-            <p className='content'>lorem ipsem set dolor sametsdfsf</p>
-        </div>
-
-        <div className='card'>
-            <img src=""></img>
-            <h4 className='d'>Detailed analysis</h4>
-            <p className='content'>lorem ipsem set dolor sametsdfsf</p>
-        </div>
-
-        <div className='card'>
-            <img src=""></img>
-            <h4 className='d'>Detailed analysis</h4>
-            <p className='content'>lorem ipsem set dolor sametsdfsf</p>
-        </div>
     </div>
+    
     
 </main>;
 }
