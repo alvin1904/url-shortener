aa14a56c0441e5441367297f28b5784653b8be5e
alvin1904 <alvin19official@gmail.com>
2022-12-07 13:58:17 +0530

    start logic rework && complete loading circle

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,7 @@ import { useGlobalContext } from '../context.js'
 function Main(){
 
     const [input, setInput] = useState('');
-    const [invalidLink, setInvalidLink] = useState(false)
+    const [error, setError] = useState(false)
     const [errorMsgMain, setErrorMsgMain] = useState('')
 
     const [shortLinks, setShortLinks] = useState([])
@@ -16,48 +16,39 @@ function Main(){
     const [copy, setCopy] = useState(false)
     const [copiedLink, setCopiedLink] = useState('')
 
-    const {shorten, errorMsg} = useGlobalContext();
+    const {shorten, loading} = useGlobalContext();
 
-const handleInvalidLink = () => {
-    setInvalidLink(true);
-    setErrorMsgMain("Invalid Link!")
-}
 
 const handleSubmit = (e) => {
     e.preventDefault();
-    setInvalidLink(false);
+    setError(false)
 
     const form = document.getElementById('form')
     if(form.checkValidity() && input)
-        shorten(input, shortLinks)
-    else    
-        return handleInvalidLink();  
+        shorten(input)
+    else{
+        setError(true);
+        return setErrorMsgMain("orengi poda")
+    } 
     setInput('')   
 }
 
-useEffect(()=>{
-    if(errorMsg){
-        setErrorMsgMain(errorMsg)
-        setInvalidLink(true)
-    }
-    else
-        console.log("Error cleared")
-},[errorMsg])
+const errorMsg='vere pani illadey'
 
 return <main className='bg-200'>
     <form id='form' className='input-container horizontal '>
-        <div className="vertical">
-            <input type='url' className={`input ${(invalidLink)?'danger':''}`} onChange={(e)=>{
+        <div className="vertical forloading">
+            <input type='url' className={`input ${(error)?'danger':''}`} onChange={(e)=>{
                 return setInput(e.target.value);
             }} value={input}></input>
-            <p className={`errormsg ${(invalidLink)?'':'hide'}`}>{invalidLink&&((errorMsgMain) || errorMsg)}</p>
+            <div className={`loader-1 center ${(loading)?'':'hide'}`}><span></span></div>
+            <p className={`errormsg ${(error)?'':'hide'}`}>{error&&((errorMsgMain) || errorMsg)}</p>
         </div>
         <button className='btn' onClick={handleSubmit}><span>Shorten it!</span></button>
     </form>
-
     <div className='shortened-links vertical'>
         {
-            shortLinks && shortLinks.length !== 0 && shortLinks.map((eachLink)=>{
+            shortLinks.map((eachLink)=>{
                 const {code, original, short} = eachLink;
                 return (<div key={code} className='shortened-link horizontal'>
                     <p className='link-actual'>{original}</p>
--- src/context.js
+++ src/context.js
@@ -3,45 +3,38 @@ import React, {useState, useEffect, useContext} from "react";
 const AppContext = React.createContext();
 const AppProvider = ({children}) => {
     
-    const [preparedData, setPreparedData] = useState([])
-    const [errorMsg, setErrorMsg] = useState('')
-    
-    const shorten = (link, shortLinks)=>{
-        let apiData = {}
-        fetch(`https://api.shrtco.de/v2/shorten?url=${link}`)
-            .then((res1)=> res1.json())
-            .then((res2)=> {
-                apiData = res2
-                const obj = {            
-                    code: res2.result.code,
-                    original: link,
-                    short: res2.result.short_link
-                }
-                return obj;
-            })
-            .then((res3)=>{
-                let temp = shortLinks;
-                temp.push(res3)
-                return res3;
-            })
-            .then((res4)=> setPreparedData(res4))
-            .catch((err)=>{
-                console.log(err)
-                switch(apiData.error_code){
-                    case 2: setErrorMsg("Invalid URL submitted")
-                            break;
-                    case 3: setErrorMsg("Rate limit reached. Wait a second")
-                            break;
-                    case 6: setErrorMsg("Something unknown to us is wrong")   
-                            break;
-                    default: setErrorMsg("Your Internet must be down")              
-                }
-            })
+    const [fetchToggle, setFetchToggle] = useState(false)
+    const [longLink, setLongLink] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    const shorten = (link)=>{
+        setLoading(true)
+        setLongLink(link)
+        console.log(link)
+        setFetchToggle(true)
     }
+
+    useEffect(()=>{
+        const fetchURL = () => {
+            fetch(`https://api.shrtco.de/v2/shorten?url=${longLink}`)
+                .then((res1)=> res1.json())
+                .then((res2)=> {
+                    console.log(res2)
+                })
+                .then(()=>{
+                    setLoading(false)
+                })
+                .then(()=>{
+                })
+        }    
+        if(fetchToggle)
+            fetchURL();
+        setFetchToggle(false)    
+    },[fetchToggle])
 
     return(
         <AppContext.Provider value={{shorten,
-                                    errorMsg
+                                    loading
                                     }}>
             {children}
         </AppContext.Provider>
